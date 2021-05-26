0e907bbdb67a061a520d8f3d15b47468277abab2
Alexey Karetnikov <48627239+gearoffortune@users.noreply.github.com>
2021-05-26 17:46:16 +0300

    Update build-pdf.js

--- build-pdf.js
+++ build-pdf.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { mdToPdf } = require('md-to-pdf');
  
 (async () => {
-  const pdf = await mdToPdf({ path: './src/resume/index.md' }, {dest: 'assets/resume.pdf'}).catch(console.error);
+  const pdf = await mdToPdf({ path: './src/resume/index.md' }, {dest: 'dist/assets/resume.pdf'}).catch(console.error);
  
   if (pdf) {
     fs.writeFileSync(pdf.filename, pdf.content);
